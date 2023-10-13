baf0465ddc45b5347b98fb72ff36487508073245
itdeveloping <oscar.rodriguez.alfaro@hotmail.com>
2023-10-12 20:09:59 -0600

    update bom

--- lesson05/scripts/code.js
+++ lesson05/scripts/code.js
@@ -1,31 +1,46 @@
-const favchap = document.querySelector("#favchap");
-console.log(favchap.value);
-const button = document.querySelector("button");
-console.log(button.innerHTML);
+const favChap = document.querySelector("#favChap");
+const addButton = document.querySelector("button");
 const list = document.querySelector("#list");
-favchap.focus();
-favchap.addEventListener('click', function () {
-    this.value = '';
-});
-button.addEventListener('click', function () {
-    if (favchap.value.trim() != '') {
-        const newli = document.createElement("li");
-        const deleteButton = document.createElement("button");
-        deleteButton.textContent = '❌';
-        newli.textContent = favchap.value;
-        newli.append(deleteButton);
-        list.appendChild(newli);
-        favchap.value = '';
-        deleteButton.addEventListener("click", function () {
-            list.removeChild(newli);
-            favchap.focus();
 
+favChap.focus();
+favChap.addEventListener("click", function () {
+    this.value = "";
+});
+addButton.addEventListener('click', function () {
+    if (favChap.value.trim() != '') {
+        const li = document.createElement("li");
+        const linkButton = document.createElement("input");
+        const deleteButton = document.createElement("button");
+        const buttonDiv = document.createElement("div");
+
+        buttonDiv.classList.add("button");
+
+        linkButton.setAttribute("type", "button");
+        linkButton.setAttribute("target", "_blank");
+        linkButton.value = "🔍";
+        deleteButton.textContent = '❌';
+        deleteButton.classList.add("button");
+        li.textContent = favChap.value;
+
+        let findText = li.textContent.replace(" ", "+");
+        let href = "https://www.churchofjesuschrist.org/search?lang=ase&query=" + findText + "&facet=scriptures&page=1";
+        linkButton.setAttribute("onCLick", "window.open('" + href + "','_blank')");
+
+        li.appendChild(buttonDiv);
+        buttonDiv.appendChild(linkButton);
+        buttonDiv.append(deleteButton);
+        list.appendChild(li);
+        favChap.value = '';
+
+        deleteButton.addEventListener("click", function () {
+            list.removeChild(li);
+            favChap.focus();
         })
     }
     else {
         alert("Favorite chapter is empty, please provide a book name and chapter.");
-        favchap.value = '';
-        favchap.focus();
+        favChap.value = '';
+        favChap.focus();
     }
 
 
