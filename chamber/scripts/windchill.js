569d55c269eca45c6d9762ae83dd824bad22cbde
itdeveloping <oscar.rodriguez.alfaro@hotmail.com>
2023-11-18 21:25:12 -0600

    banner

--- chamber/scripts/windchill.js
+++ chamber/scripts/windchill.js
@@ -5,4 +5,16 @@ btnCalculate.addEventListener("click", function (event) {
     let windChill = (35.74 + (0.6215 * temperature.value)) - (35.75 * Math.pow(windSpeed.value, 0.16)) + (0.4275 * temperature.value * Math.pow(windSpeed.value, 0.16));
     document.querySelector("#windChill").value = Math.round(windChill);
 })
+const banner = document.querySelector(".banner");
 
+const bannerClose = document.querySelector("#bannerClose");
+bannerClose.addEventListener("click", function () {
+    banner.style.display = "none";
+});
+const today = new Date();
+
+if (today.getDay() >= 1 && today.getDay() <= 3) {
+    banner.style.display = "block";
+}
+else
+    banner.style.display = "none";
