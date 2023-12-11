7c59d4b78f2cf2527b8424a246e102a0b2e5623b
itdeveloping <oscar.rodriguez.alfaro@hotmail.com>
2023-12-11 11:36:03 -0600

    update

--- scoots/scripts/main.js
+++ scoots/scripts/main.js
@@ -1,3 +1,5 @@
+
+//code for rentals table
 const baseURL = "https://itdeveloping.github.io/wdd230/";
 const RentalsURL = "https://itdeveloping.github.io/wdd230/scoots/data/rentals.json";
 //const RentalsURL = "data/rentals.json"; //for local test
@@ -10,7 +12,6 @@ async function getRentals() {
 getRentals();
 const displayRentals = (Rentals) => {
     const table = document.querySelector("#rentals");
-
 
     //console.log(data.RentalType[0].Type);
     //console.log(Rentals);
@@ -48,6 +49,7 @@ const displayRentals = (Rentals) => {
     });
 }
 
+//code for responsive menu
 const menu = document.querySelector('#menu');
 const navigation = document.querySelector('.nav-bar');
 
@@ -56,7 +58,7 @@ menu.addEventListener('click', () => {
     menu.classList.toggle('open');
 });
 
-
+//code for dark/light mode
 const modeButton = document.querySelector("#mode");
 const body = document.querySelector("body");
 
@@ -73,3 +75,76 @@ modeButton.addEventListener("click", function () {
 
     }
 });
+
+//code for weather and forecast
+async function getCurrentWeather() {
+    const url = 'https://api.openweathermap.org/data/2.5/weather?lat=20.4255199&lon=-87.0284159&tz=-06&appid=4381b2da6ac807913d2efec6ef1c1d4f&units=metric';
+    try {
+        const response = await fetch(url);
+        if (response.ok) {
+            const data = await response.json();
+            displayCurrentWeather(data);
+        } else {
+            throw Error(await response.text());
+        }
+    } catch (error) {
+        console.log(error);
+    }
+}
+async function getForecast() {
+    const url = 'https://api.openweathermap.org/data/2.5/forecast?lat=20.4255199&lon=-87.0284159&tz=-06&appid=4381b2da6ac807913d2efec6ef1c1d4f&units=metric';
+    try {
+        const response = await fetch(url);
+        if (response.ok) {
+            const data = await response.json();
+            displayForecast(data);
+        } else {
+            throw Error(await response.text());
+        }
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+
+function displayCurrentWeather(data) {
+    console.log(data);
+    const weatherDiv = document.querySelector('.local-weather');
+    const currentTemp = document.createElement('p');
+    const image = document.createElement('img');
+    const element = data;
+    let desc = `${element.weather[0].description} | humidity: ${element.main['humidity']}% `;
+    const iconsrc = `https://openweathermap.org/img/w/${element.weather[0].icon}.png`;
+    image.setAttribute('src', iconsrc);
+    image.setAttribute('alt', element.weather[0].description);
+    currentTemp.innerHTML = `Temp: ${element.main.temp}&deg;C | ${desc.toUpperCase()} <br>`;
+
+    currentTemp.appendChild(image);
+    weatherDiv.appendChild(currentTemp);
+
+}
+function displayForecast(data) {
+
+    //console.log(data);
+    const forecastDiv = document.querySelector('.local-forecast');
+    const forecastTemp = document.createElement('p');
+    const forecastImage = document.createElement('img');
+    const forecastElement = data.list[7];
+
+    //console.log(); // testing only
+    //console.log(new Date(element.dt));
+    //console.log(element.dt_txt); // testing only
+    let forecastDesc = `${forecastElement.weather[0].description} | humidity: ${forecastElement.main['humidity']}% `;
+    const forecastIconsrc = `https://openweathermap.org/img/w/${forecastElement.weather[0].icon}.png`;
+    forecastImage.setAttribute('src', forecastIconsrc);
+    forecastImage.setAttribute('alt', forecastElement.weather[0].description);
+    var options = { weekday: 'long' };
+    forecastTemp.innerHTML = new Date(forecastElement.dt_txt).toLocaleDateString("en-US", options) + ` | Temp: ${forecastElement.main.temp}&deg;C ${forecastDesc.toUpperCase()} <br>`;
+
+    forecastTemp.appendChild(forecastImage);
+    forecastDiv.appendChild(forecastTemp);
+
+}
+getCurrentWeather();
+
+getForecast();
