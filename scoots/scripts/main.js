7bdaf21ba9eb8d474690db3248a3f18ea2e516a6
itdeveloping <oscar.rodriguez.alfaro@hotmail.com>
2023-12-11 14:51:16 -0600

    update

--- scoots/scripts/main.js
+++ scoots/scripts/main.js
@@ -1,53 +1,58 @@
+const url = window.location.pathname;
+const filename = url.substring(url.lastIndexOf('/') + 1);
 
-//code for rentals table
-const baseURL = "https://itdeveloping.github.io/wdd230/";
-const RentalsURL = "https://itdeveloping.github.io/wdd230/scoots/data/rentals.json";
-//const RentalsURL = "data/rentals.json"; //for local test
-async function getRentals() {
-    const response = await fetch(RentalsURL);
-    const data = await response.json();
-    displayRentals(data.RentalType);
+if (filename == 'rentals.html') {
+    //code for rentals table
+    const baseURL = "https://itdeveloping.github.io/wdd230/";
+    const RentalsURL = "https://itdeveloping.github.io/wdd230/scoots/data/rentals.json";
+    //const RentalsURL = "data/rentals.json"; //for local test
+    async function getRentals() {
+        const response = await fetch(RentalsURL);
+        const data = await response.json();
+        displayRentals(data.RentalType);
+    }
+
+    getRentals();
+    const displayRentals = (Rentals) => {
+        const table = document.querySelector("#rentals");
+
+        //console.log(data.RentalType[0].Type);
+        //console.log(Rentals);
+
+        Rentals.forEach(item => {
+
+            const tr = document.createElement('tr');
+
+            const td1 = document.createElement('td');
+            td1.textContent = item.Type;
+            tr.appendChild(td1);
+
+            const td2 = document.createElement('td');
+            td2.textContent = item.Max;
+            tr.appendChild(td2);
+
+            const td3 = document.createElement('td');
+            td3.textContent = item.Res3hr;
+            tr.appendChild(td3);
+
+            const td4 = document.createElement('td');
+            td4.textContent = item.Resfullday;
+            tr.appendChild(td4);
+
+            const td5 = document.createElement('td');
+            td5.textContent = item.Wi3hr;
+            tr.appendChild(td5);
+
+            const td6 = document.createElement('td');
+            td6.textContent = item.Wifullday;
+            tr.appendChild(td6);
+
+            table.appendChild(tr);
+
+        });
+    }
 }
 
-getRentals();
-const displayRentals = (Rentals) => {
-    const table = document.querySelector("#rentals");
-
-    //console.log(data.RentalType[0].Type);
-    //console.log(Rentals);
-
-    Rentals.forEach(item => {
-
-        const tr = document.createElement('tr');
-
-        const td1 = document.createElement('td');
-        td1.textContent = item.Type;
-        tr.appendChild(td1);
-
-        const td2 = document.createElement('td');
-        td2.textContent = item.Max;
-        tr.appendChild(td2);
-
-        const td3 = document.createElement('td');
-        td3.textContent = item.Res3hr;
-        tr.appendChild(td3);
-
-        const td4 = document.createElement('td');
-        td4.textContent = item.Resfullday;
-        tr.appendChild(td4);
-
-        const td5 = document.createElement('td');
-        td5.textContent = item.Wi3hr;
-        tr.appendChild(td5);
-
-        const td6 = document.createElement('td');
-        td6.textContent = item.Wifullday;
-        tr.appendChild(td6);
-
-        table.appendChild(tr);
-
-    });
-}
 
 //code for responsive menu
 const menu = document.querySelector('#menu');
@@ -108,7 +113,23 @@ async function getForecast() {
 
 
 function displayCurrentWeather(data) {
-    console.log(data);
+    //console.log();
+
+    const weatherAd = document.querySelector('.weather-ad');
+    if (filename == 'index.html') {
+        weatherAd.style.display = "block";
+        const tempMax = data.main['temp_max'];
+        const weatherAdP = document.querySelector('.weather-ad p');
+        weatherAdP.textContent = `Cozumel's high temperature for today is ${tempMax}°C`;
+        const btnClose = document.querySelector('.close-weather-ad');
+        btnClose.addEventListener("click", function () {
+            weatherAd.style.display = "none";
+            btnClose.setAttribute("title", "Click to close");
+        });
+    }
+    else {
+        weatherAd.style.display = "none";
+    }
     const weatherDiv = document.querySelector('.local-weather');
     const currentTemp = document.createElement('p');
     const image = document.createElement('img');
@@ -146,5 +167,6 @@ function displayForecast(data) {
 
 }
 getCurrentWeather();
-
 getForecast();
+
+
